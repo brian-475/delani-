58fa88028b0172f05036b76ac45fd575397b1f3b
brian-475 <brianadala15@gmail.com>
2020-02-24 11:45:11 +0300

    at 1144hrs am commiting my new js files

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 $("document").ready(function(){
-    $(".desn").click(function(){
-        $("#des-image").hide();
-        $(".lion").hide();
-        $(".desn").show();
-        $("#des-image").show();
+    $(".desn-2").click(function(){
+        $("#twenty").toggle();
     })
 })
 $("document").ready(function(){
-    $(".desn-2").click(function(){
-        $("#des-image-2").hide();
-        $(".dev").hide();
-        $(".desn-2").show();
-        $("#des-image-2").show();
+    $(".fear").click(function(){
+        $("#building").toggle();
     })
 })
-$("document").ready(function (){
-    $("#planning").click(function(){
-        $(".building").hide();
-    })
-        $("planning").click(function() {
-        $(".building").show();
-    })
-})
+$("document").ready(function(){
+    $(".col-md-4").click(function(){
+    $("#planning").toggle();
+    $(".fear").toggle();
+    });
+});
+
+$("document").ready(function(){
+    $("#p").hover(function(){
+        $("#pp").addClass("class1");   
+    
+});
+});
